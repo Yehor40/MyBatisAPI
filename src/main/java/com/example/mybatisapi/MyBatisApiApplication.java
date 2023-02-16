91473c8fe4204f94ee0a6682c3115e8b5bdfc173
Yehor40 <fraugrey3@gmail.com>
2023-02-16 14:36:20 +0100

    Basic api with MyBatis

---
+++ src/main/java/com/example/mybatisapi/MyBatisApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.mybatisapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBatisApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBatisApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mybatisapi/MyBatisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mybatisapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBatisApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
